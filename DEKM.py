93b2a4c43b33b04441ac3913e9d3f39554883b47
helloworld <30624620+spdj2271@users.noreply.github.com>
2021-10-01 22:02:04 +0800

    Update DEKM.py

--- DEKM.py
+++ DEKM.py
@@ -1,101 +1,15 @@
-import h5py
 import numpy as np
 import tensorflow as tf
 from sklearn.cluster import KMeans
 from tensorflow.keras import layers
 from tensorflow.keras import losses
-from tensorflow.keras.datasets import mnist
 from tensorflow.keras.models import Model
-
-
-def get_ACC_NMI(y, y_pred):
-    s = np.unique(y_pred)
-    t = np.unique(y)
-
-    N = len(np.unique(y_pred))
-    C = np.zeros((N, N), dtype=np.int32)
-    for i in range(N):
-        for j in range(N):
-            idx = np.logical_and(y_pred == s[i], y == t[j])
-            C[i][j] = np.count_nonzero(idx)
-    Cmax = np.amax(C)
-    C = Cmax - C
-    from scipy.optimize import linear_sum_assignment
-    row, col = linear_sum_assignment(C)
-    count = 0
-    for i in range(N):
-        idx = np.logical_and(y_pred == s[row[i]], y == t[col[i]])
-        count += np.count_nonzero(idx)
-    acc = np.round(1.0 * count / len(y), 5)
-
-    temp = np.array(y_pred)
-    for i in range(N):
-        y_pred[temp == col[i]] = i
-    from sklearn.metrics import normalized_mutual_info_score
-    nmi = np.round(normalized_mutual_info_score(y, y_pred), 5)
-    return acc, nmi
-
-
-def get_xy(ds_type='MNIST', x_flatten=False, is_print=True):
-    if ds_type == 'MNIST':
-        (x_train, y_train), (x_test, y_test) = mnist.load_data()
-        x = np.concatenate((x_train, x_test))
-        y = np.concatenate((y_train, y_test))
-        x = np.expand_dims(np.divide(x, 255.), -1)
-    elif ds_type == 'USPS':
-        with h5py.File('USPS.h5', 'r') as hf:
-            train = hf.get('train')
-            X_tr = train.get('data')[:]
-            y_tr = train.get('target')[:]
-            test = hf.get('test')
-            X_te = test.get('data')[:]
-            y_te = test.get('target')[:]
-        x = np.concatenate([X_tr, X_te], 0)
-        y = np.concatenate([y_tr, y_te], 0)
-        x = np.reshape(x, (len(x), 16, 16)).astype(np.float32)
-    elif ds_type == 'COIL20':
-        f = h5py.File('COIL20.h5', 'r')
-        x = np.array(f['data'][()]).squeeze()
-        x = np.expand_dims(np.swapaxes(x, 1, 2).astype(np.float32), -1)
-        x = x / 255.
-        y = np.array(f['labels'][()]).astype(np.float32)
-        y[y == 20.] = 0.
-        index = np.random.permutation(len(x))
-        x = x[index]
-        y = y[index]
-        x = tf.image.resize(x, [28, 28]).numpy()
-        # with h5py.File('COIL_2.h5', 'r') as hf:
-        #     x = hf.get('X')[:]
-        #     y = hf.get('Y')[:]
-        #     x = x / 255.
-        #     y[y == 20] = 0
-        #     index = np.random.permutation(len(x))
-        #     x = x[index]
-        #     y = y[index]
-        #     x = tf.image.resize(tf.expand_dims(x, -1), [28, 28]).numpy()
-    elif ds_type == 'FRGC':
-        with h5py.File('FRGC.h5', 'r') as hf:
-            data = hf.get('data')[:]
-            data = np.swapaxes(data, 1, 3)
-            x = data / 255.
-            y = hf.get('labels')[:]
-            y_unique = np.unique(y)
-            for i in range(len(y_unique)):
-                y[y == y_unique[i]] = i
-            index = np.random.permutation(len(x))
-            x = x[index]
-            y = y[index]
-    if x_flatten:
-        x = x.reshape((x.shape[0], -1))
-    if is_print:
-        print(ds_type)
-    return x, y
-
-
-def get_dataset_xx(ds_type='MNIST', x_flatten=False, is_print=True):
-    x, _ = get_xy(ds_type=ds_type, x_flatten=x_flatten, is_print=is_print)
-    ds = tf.data.Dataset.from_tensor_slices((x, x))
-    return ds
+from utils import get_ACC_NMI
+from utils import get_xy
+from utils import log_csv
+import time
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def model_conv(load_weights=True):
@@ -114,8 +28,7 @@ def model_conv(load_weights=True):
     x = layers.Conv2D(filters[2], kernel_size=3, strides=2, padding=pad3, activation='relu', kernel_initializer=init)(x)
     x = layers.Flatten()(x)
     x = layers.Dense(units=filters[-1], name='embed')(x)
-    # x=layers.BatchNormalization()(x)
-#     x = tf.divide(x, tf.expand_dims(tf.norm(x, 2, -1), -1))
+    x = tf.divide(x, tf.expand_dims(tf.norm(x, 2, -1), -1))
     h = x
     x = layers.Dense(filters[2] * (input_shape[0] // 8) * (input_shape[0] // 8), activation='relu')(x)
     x = layers.Reshape((input_shape[0] // 8, input_shape[0] // 8, filters[2]))(x)
@@ -127,38 +40,13 @@ def model_conv(load_weights=True):
     model = Model(inputs=input, outputs=output)
     # model.summary()
     if load_weights:
-        model.load_weights('exp.h5')
+        model.load_weights(f'weight_base_l2_{ds_name}.h5')
         print('model_conv: weights was loaded')
     return model
 
 
-def loss_train_base(y_true, y_pred):
-    y_true = layers.Flatten()(y_true)
-    y_pred = y_pred[:, hidden_units:]
-    return losses.mse(y_true, y_pred)
-
-
-def train_base():
-    model = model_conv(load_weights=False)
-    model.compile(optimizer='adam', loss=loss_train_base)
-    # model.compile(optimizer=tf.keras.optimizers.SGD(0.1,0.9), loss=loss_train_base)
-    ds = get_dataset_xx(ds_type=ds_name).shuffle(8000).batch(pretrain_batch_size)
-    model.fit(ds, epochs=pretrain_epochs, verbose=2)
-    # for i in range(pretrain_epochs):
-    #     ds = get_dataset_xx(ds_type=ds_name).shuffle(8000).batch(pretrain_batch_size)
-    #     for image, _ in ds:
-    #         random = np.random.uniform()
-    #         if random < 0.2:
-    #             image = tf.image.random_contrast(image, 0.2, 0.5)
-    #         elif random < 0.5:
-    #             image = tf.image.random_brightness(image, 0.2)
-    #         loss = model.train_on_batch(image, image)
-    #     print(i, loss)
-    model.save_weights('exp.h5')
-
-
 def show_reconstruct():
-    x, y = get_xy(ds_type=ds_name)
+    x, y = get_xy(ds_name=ds_name)
     import matplotlib.pyplot as plt
     i = 1
     model = model_conv()
@@ -180,6 +68,19 @@ def show_reconstruct():
     plt.show()
 
 
+def loss_train_base(y_true, y_pred):
+    y_true = layers.Flatten()(y_true)
+    y_pred = y_pred[:, hidden_units:]
+    return losses.mse(y_true, y_pred)
+
+
+def train_base(ds_xx):
+    model = model_conv(load_weights=False)
+    model.compile(optimizer='adam', loss=loss_train_base)
+    model.fit(ds_xx, epochs=pretrain_epochs, verbose=2)
+    model.save_weights(f'weight_base_l2_{ds_name}.h5')
+
+
 def sorted_eig(X):
     e_vals, e_vecs = np.linalg.eig(X)  # 特征向量v[:,i]对应特征值w[i]，即每一列每一个特征向量
     idx = np.argsort(e_vals)
@@ -188,19 +89,23 @@ def sorted_eig(X):
     return e_vals, e_vecs
 
 
-def train():
+def train(x, y):
+    log_str = f'iter; acc, nmi, ri ; loss; n_changed_assignment; time:{time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())}'
+    log_csv(log_str.split(';'), file_name=ds_name)
     model = model_conv()
-    x, y = get_xy(ds_type=ds_name)
+
     optimizer = tf.keras.optimizers.Adam()
-    update_interval = 40
     loss_value = 0
     index = 0
+    kmeans_n_init = 100
     assignment = np.array([-1] * len(x))
     index_array = np.arange(x.shape[0])
     for ite in range(int(140 * 100)):
         if ite % update_interval == 0:
             H = model(x).numpy()[:, :hidden_units]
-            ans_kmeans = KMeans(n_clusters=n_clusters, n_init=500).fit(H)
+            ans_kmeans = KMeans(n_clusters=n_clusters, n_init=kmeans_n_init).fit(H)
+            kmeans_n_init = int(ans_kmeans.n_iter_ * 2)
+
             U = ans_kmeans.cluster_centers_
             assignment_new = ans_kmeans.labels_
 
@@ -212,7 +117,7 @@ def train():
             temp = np.array(assignment)
             for i in range(n_clusters):
                 assignment[temp == ind[1][i]] = i
-            change_assignment = np.sum(assignment_new != assignment)
+            n_change_assignment = np.sum(assignment_new != assignment)
             assignment = assignment_new
 
             S_i = []
@@ -228,10 +133,15 @@ def train():
             #
             loss = np.round(np.mean(loss_value), 5)
             acc, nmi = get_ACC_NMI(np.array(y), np.array(assignment))
-            print('ite %2d ,acc: %.5f, nmi: %.5f, loss:%.5f' % (ite // update_interval, acc, nmi, loss),
-                  np.round(Evals[-5:], 2).astype(np.int), change_assignment,'time:',time.time()-time_start)
-        if change_assignment<=len(x)*0.005:
-            model.save_weights('final_DK.h5')
+
+            # log
+            log_str = f'iter {ite // update_interval}; acc, nmi, ri = {acc, nmi, loss}; loss:' \
+                      f'{loss:.5f}; n_changed_assignment:{n_change_assignment}; time:{time.time() - time_start:.3f}'
+            print(log_str)
+            log_csv(log_str.split(';'), file_name=ds_name)
+
+        if n_change_assignment <= len(x) * 0.005:
+            model.save_weights(f'weight_final_l2_{ds_name}.h5')
             print('end')
             break
         idx = index_array[index * batch_size: min((index + 1) * batch_size, x.shape[0])]
@@ -244,7 +154,7 @@ def train():
             tape.watch(model.trainable_variables)
             y_pred = model(x[idx])
             y_pred_cluster = tf.matmul(y_pred[:, :hidden_units], V)
-            loss_value = losses.mse(y_true, y_pred_cluster)+losses.mse(layers.Flatten()(x[idx]),y_pred[:,hidden_units:])
+            loss_value = losses.mse(y_true, y_pred_cluster)
         grads = tape.gradient(loss_value, model.trainable_variables)
         optimizer.apply_gradients(zip(grads, model.trainable_variables))
 
@@ -255,38 +165,25 @@ if __name__ == '__main__':
     pretrain_epochs = 200
     pretrain_batch_size = 256
     batch_size = 256
-    import argparse
-
-    parser = argparse.ArgumentParser(description='select dataset:MNIST,COIL20,FRGC,USPS')
-    parser.add_argument('ds_name', type=str)
-
-    args = parser.parse_args()
-    if args.ds_name in ['MNIST','USPS','FRGC','COIL20']:
-        ds_name=args.ds_name
-    else:
-        ds_name='MNIST'
-
+    update_interval = 40
+    hidden_units = 10
+    ds_name = 'FRGC'
     if ds_name == 'MNIST':
         input_shape = (28, 28, 1)
         n_clusters = 10
-        hidden_units = n_clusters
     elif ds_name == 'USPS':
         input_shape = (16, 16, 1)
         n_clusters = 10
-        hidden_units = n_clusters
     elif ds_name == 'COIL20':
-        pretrain_batch_size = 32
-        batch_size = 32
         input_shape = (28, 28, 1)
         n_clusters = 20
-        hidden_units = n_clusters
     elif ds_name == 'FRGC':
         input_shape = (32, 32, 3)
         n_clusters = 20
-        hidden_units = n_clusters
-    import time
-    time_start=time.time()
-    train_base()
-    show_reconstruct()
-    train()
-    print(time.time()-time_start)
+
+    time_start = time.time()
+    x, y = get_xy(ds_name=ds_name)
+    ds_xx = tf.data.Dataset.from_tensor_slices((x, x)).shuffle(8000).batch(pretrain_batch_size)
+    train_base(ds_xx)
+    train(x, y)
+    print(time.time() - time_start)
